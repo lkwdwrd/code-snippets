c91407ecf8e27ebcb6819d666891d104a25a1493
Luke Woodward <luke.woodward@10up.com>
2014-03-23 01:42:30 -0700

    Adding close helper function to the echo object

--- assets/js/src/echo.js
+++ assets/js/src/echo.js
@@ -178,11 +178,19 @@
 		};
 	})());
 
+	// Helper functions for open and close.
 	function open() {
 		if ( 'undefined' === typeof modal ) {
 			modal = new echo.Modal();
 		}
 		modal.open();
 	}
+	function close() {
+		if ( 'undefined' === typeof modal || 'function' !== typeof modal.close ) {
+			return;
+		}
+		modal.close();
+	}
 	echo.open = open;
+	echo.close = close;
 } )( this );
