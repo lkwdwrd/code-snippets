25255e649d44a53f6c54c40da5ac3a3ff0e1defa
Luke Woodward <luke.woodward@10up.com>
2014-03-23 00:11:51 -0700

    Build out the modal system further

--- assets/js/src/echo.js
+++ assets/js/src/echo.js
@@ -17,60 +17,84 @@
 	echo.Controller = media.controller.State.extend((function(){
 
 		function init( options ) {
-
+			this.set( 'library', new Backbone.Collection() );
+			this.set( 'selection', new Backbone.Collection() );
 		}
 		return {
 			defaults: {
 				id:         'codeLibrary',
-				toolbar:    'select',
+				toolbar:    'insert',
+				router:     'tabs',
 				sidebar:    'settings',
-				content:    'upload',
-				router:     'browse',
+				content:    'codeLibrary',
 				menu:       'default',
-				title:      'Code Snippets',
-
-				// Uses a user setting to override the content mode.
-				contentUserSetting: true,
-
-				// Sync the selection from the last state when 'multiple' matches.
-				syncSelection: true
+				title:      'Code Jots',
+				select:      true
 			},
-			init: init
+			initialize: init
 		};
 	})());
 	echo.Modal = media.view.MediaFrame.extend((function(){
 		function init() {
 			media.view.MediaFrame.prototype.initialize.apply( this, arguments );
-			
+			this.on( 'all', function( action ){ console.log( action ); } );
 			_.defaults( this.options, {
-				state:    'codeLibrary'
+				state:    'codeLibrary',
+				button:   {
+					text: 'Insert Jot'
+				}
 			});
 
-			this.createStates();
+			// Register our controller
+			this.states.add([
+				new echo.Controller()
+			]);
+			// Do stuff when stuff happens!
 			this.bindHandlers();
 		}
 
-		function createStates() {
-			var options = this.options;
-
-			if ( this.options.states ) {
-				return;
-			}
-
-			// Add the default states.
-			this.states.add([
-				// Main states.
-				new echo.Controller()
-			]);
-		}
 		function bindHandlers() {
-			
+			this.on( 'router:create:tabs',         this.createRouter,   this );
+			this.on( 'router:render:tabs',         this.renderRouter,   this );
+			this.on( 'content:create:codeLibrary', this.libraryContent, this );
+			this.on( 'content:render:codeEdit',    this.editContent,    this );
+			this.on( 'toolbar:create:insert',      this.createToolbar,  this );
+		}
+
+		function renderRouter( view ) {
+			view.set({
+				codeLibrary: {
+					text:     'Jot Library',
+					priority: 20
+				},
+				codeEdit: {
+					text:     'Edit Jot',
+					priority: 40
+				}
+			});
+		}
+
+		function libraryContent( content ) {
+			content = new media.View();
+		}
+
+		function editContent( content ) {
+			content = new media.View();
+		}
+
+		function createToolbar( toolbar, options ) {
+			options = options || this.options.button || {};
+			options.controller = this;
+
+			toolbar.view = new media.view.Toolbar.Select( options );
 		}
 
 		return {
-			initialize: init,
-			createStates: createStates,
-			bindHandlers: bindHandlers
+			initialize:    init,
+			bindHandlers:  bindHandlers,
+			renderRouter:  renderRouter,
+			createToolbar: createToolbar
+
 		};
 	})());
 
